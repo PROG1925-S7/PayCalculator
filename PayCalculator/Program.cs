e1c6156bb23d4de10802f9e72158f74a69eaf314
Tony Theodoropoulos <theodot@outlook.com>
2023-01-25 18:40:19 -0500

    Add project files.

---
+++ PayCalculator/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // variable declaration
+            string firstName;
+            double hours;
+            double payRate;
+            double preTax;
+            double taxRate = 0.25;
+            double taxAmount;
+            double earned;
+
+            //get inputs
+            Console.Write("Please enter your first name: ");
+            firstName = Console.ReadLine();
+
+            Console.Write("Please enter hours worked: ");
+            hours = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Please enter your rate of pay: ");
+            payRate = Convert.ToDouble(Console.ReadLine());
+
+            //do calcuations
+            preTax = hours * payRate; 
+            taxAmount = preTax * taxRate;
+            earned = preTax - taxAmount;
+
+            earned = Math.Round(earned, 2); 
+
+            //show outputs
+            Console.WriteLine($"Pay for {firstName}\n");
+            Console.WriteLine($"Hours worked:   {hours}");
+            Console.WriteLine($"Rate of Pay:    {payRate.ToString("C")}");
+            Console.WriteLine($"Pre Tax amount: {preTax.ToString("C")}");
+            Console.WriteLine($"Tax Amount:     {taxAmount.ToString("C")}");
+            Console.WriteLine($"Total Earned:   {earned.ToString("C")}");      
+            
+            Console.ReadKey();
+        }
+    }
+}
